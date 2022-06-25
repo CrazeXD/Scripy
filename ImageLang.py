8916ab0c68bb2ef948f967f81f361e20b0e073b2
Craze <CrazeXD@github.com>
2022-06-25 15:43:41 -0700

    ..

--- ImageLang.py
+++ ImageLang.py
@@ -1,3 +1,4 @@
+from ast import Try
 import sys
 from PIL import Image
 import numpy as np
@@ -34,14 +35,9 @@ class ImageRender:
         for index, row in enumerate(self._compressedValuesDefault):
             if row == "ENDREN":
                 if index!=len(self._compressedValuesDefault)-1:
-                    self.existscommands = True
                     self._compressedValuesDefault = self._compressedValuesDefault[:index]
                     self._compressedValuesDefault.pop(-1)
-                    self.commandIndexStartInclusive = index+1
-                    self.commandListRows = self._compressedValuesDefault[index+1:]
                     break
-                else:
-                    self.existscommands = False #Are commands there
             elif index+1 == len(self._compressedValuesDefault) and row != "ENDREN":
                 if self._printing != False:
                     print("EOS Error: No end on image render. Assuming end of file is the end of the image...")
@@ -118,57 +114,20 @@ class ImageRender:
         new_image.save('new.png')
         print("Render completed.")
 
-class ImageCommands:
-
-    def crop(self, crops):
-        self._im.crop(crops)
-    def rotate(self, theta):
-        self._im.rotate(theta)
-    def evalcomands(self):
-        for index, i in enumerate(self.commands):
-            commandlist = {"ROTATE": "self.rotate(commandparams)", "CROP": "self.crop(commandparams)"}
-            for command in commandlist:
-                if command in i:
-                    commandparams = i[len(command)+1:]
-                    try:
-                        eval(commandlist[command])
-                    except:
-                        print(f"Fatal error: Line {index+self.commandindexstart} -> Invalid arguments.")
-                else:
-                    pass
-
-    def __init__(self, imagePath, commands = None, file = None, commandindexstart = 0):
-        if commands == None and file == None:
-            print("Fatal Error: No base input provided for image altercation.")
-            exit()
-        elif any([commands, file] == ""):
-            print("Fatal error: Filepath is empty.")
-            exit()
-        if commands == None:
-            filer = file.open()
-            self.commands = filer.readlines()
-            filer.close()
-            self.commands.pop(0) #COMMANDS
-            for index, j in enumerate(self.commands):
-                if j == "ENDCOMMANDS":
-                    endindex = index
-            if not any(self.commands == "ENDCOMMANDS"):
-                print("EOS Error: No end on image commands. Assuming end of file is the end of the commands...")
-            self.commands.pop(endindex) #ENDCOMMANDS
-        elif file == None:
-            self.commands = commands
-        self._im = Image.open(imagePath)
-        
-        
+def invokeIMREN(file):
+    starter = ImageRender(file)
+    print(starter.parse())
+    starter.addtoobj()
+    starter.render()
 
 if __name__ == "__main__":
     try: 
-       file = r"{}".format(sys.argv[1])
+       filepath = r"{}".format(sys.argv[1])
     except IndexError:
         print("Fatal error: Missing input file.")
         exit()
-    if file.endswith(".code"):
-        opened = open(file, "r", encoding="utf-8")
+    if filepath.endswith(".code"):
+        opened = open(filepath, "r", encoding="utf-8")
     else:
         print("Fatal error: Invalid input file.")
         exit()
@@ -181,7 +140,4 @@ if __name__ == "__main__":
         type = readlines[0]
     print(type)
     if type == "IMREN\n":
-        starter = ImageRender(file)
-        print(starter.parse())
-        starter.addtoobj()
-        starter.render()
+        invokeIMREN(filepath)
